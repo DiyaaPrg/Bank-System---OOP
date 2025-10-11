a6ede772a3fa5e6de7fdbd9ffd8f214bc5aa0398
Diyaa <141173873+DiyaaPrg@users.noreply.github.com>
2025-10-11 10:38:11 +0100

    Add files via upload

---
+++ Include/Authentication/clsLoginRegisterScreen.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include "clsScreen.h"
+#include "clsBankClient.h"
+#include "clsString.h"
+#include <iomanip>
+#include "clsUser.h"
+
+class clsLoginRegisterScreen : protected clsScreen
+{
+    static void _PrintDataLine(clsUser::stLoginRegister record)
+    {
+        cout << setw(8) << left << "" << "| " << setw(30) << left << record.DateTime;
+        cout << "| " << setw(20) << left << record.UserName;
+        cout << "| " << setw(15) << left << record.Password;
+        cout << "| " << setw(10) << left << record.Permissions;
+    }
+
+public:
+
+    static void ShowRegisterList()
+    {
+
+        vector <clsUser::stLoginRegister> vLoginRegister = clsUser::GetRegisterList();
+
+        string Title = "\t Login Register List Screen";
+        string SubTitle = "\t    (" + to_string(vLoginRegister.size()) + ") Record(s).";
+
+        _DrawScreenHeader(Title, SubTitle);
+
+
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+        cout << setw(8) << left << "" << "| " << left << setw(30) << "Date/Time";
+        cout << "| " << left << setw(20) << "UserName";
+        cout << "| " << left << setw(15) << "Password";
+        cout << "| " << left << setw(10) << "Permessions";
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+        if (vLoginRegister.size() == 0)
+            cout << "\t\t\t\tNo Logins Available In the System!";
+        else
+
+            for (clsUser::stLoginRegister record : vLoginRegister)
+            {
+                _PrintDataLine(record);
+                cout << endl;
+            }
+
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+    }
+};
+
